73e1e6e4b0f82f2f1912c48632704e798223dc73
Valerian Cubero <nekith@gmail.com>
2015-07-12 13:38:23 +0200

    items bouncing animation

---
+++ Assets/Scripts/BounceAnimation.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BounceAnimation : MonoBehaviour
+{
+	public Vector3 offset;
+	public float period;
+	public float lerpRate;
+	
+	private Vector3 _highestPosition;
+	private Vector3 _lowestPosition;
+	private float _timer;
+	
+	void Start()
+	{
+		_lowestPosition = transform.position;
+		_highestPosition = transform.position + offset;
+	}
+	
+	void Update()
+	{
+		_timer += Time.deltaTime;
+		if (_timer > period) {
+			_timer -= period;
+		} else if (_timer > period / 2) {
+			transform.position = Vector3.Lerp(transform.position, _lowestPosition, lerpRate * Time.deltaTime);
+		} else {
+			transform.position = Vector3.Lerp(transform.position, _highestPosition, lerpRate * Time.deltaTime);
+		}
+	}
+}
