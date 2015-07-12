5b59b4f5d24b4bb951373a40d493579d49bb25ce
jules.briquet <jules.briquet@gmail.com>
2015-07-12 15:19:12 +0200

    Adding bar

---
+++ Assets/Scripts/UI/PowerBar.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PowerBar : MonoBehaviour {
+
+	public PlayableCharacter playChar;
+	private RectTransform rect;
+
+	// Use this for initialization
+	void Start () {
+		rect = gameObject.GetComponent<RectTransform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float ratio = (playChar.remainingTime / playChar.powerUpCooldown) * 100f;
+		rect.sizeDelta = new Vector2(ratio, rect.sizeDelta.y);
+	}
+}
